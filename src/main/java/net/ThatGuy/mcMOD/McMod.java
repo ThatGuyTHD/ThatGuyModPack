53cd8c0b4c92050b81d42d9dff86c808f1c8e07c
ThatGuy <thatuhui@gmail.com>
2024-06-18 15:38:49 -0700

    just made some changes

--- src/main/java/net/ThatGuy/mcMOD/McMod.java
+++ src/main/java/net/ThatGuy/mcMOD/McMod.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
+
 @Mod(McMod.MOD_ID)
 public class McMod {
     public static final String MOD_ID = "safteybunkermod";
